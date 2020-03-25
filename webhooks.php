941e0cc0fd3992b76fb1c9e096b1b33c2dc040a6
ipoolnatt <48254772+ipoolnatt@users.noreply.github.com>
2020-03-25 20:49:39 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'y3PfB/2JZeWmRwK5O8WGKZLPXe3VQkHZSCiSlcDb9kTMANnz0lo1viXxf0ru3EVmp69wzCM9SsX6k1lB1d4Gs6KDUmFgfWo/jE83ktX8GPLiByJ4Gs1pWTi16XBPSvHQC1PxJILLlbn/lZtS6XFpkwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
