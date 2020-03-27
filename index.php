2d6cbf2e69ef47254e21c893c2d61f11a8cd963b
ipoolnatt <48254772+ipoolnatt@users.noreply.github.com>
2020-03-27 17:29:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,10 @@
     $message = $arrayJson['events'][0]['message']['text'];
 #ตัวอย่าง Message Type "Text"
     if($message == "สวัสดี"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "displayName";
+        //$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['replyToken'] = $arrayJson['soure']['userId'];
+        //$arrayPostData['messages'][0]['type'] = "text";
+        //$arrayPostData['messages'][0]['text'] = "displayName";
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Sticker"
